7c61867bd78c58fc0dfa19b696afcdfa55e0c03a
Yihang Wang <yihangwang1020@gmail.com>
2025-04-10 18:47:58 +0800

    Update trainer.py

--- rag_retrieval/train/embedding/trainer.py
+++ rag_retrieval/train/embedding/trainer.py
@@ -111,7 +111,7 @@ class Trainer:
                         save_dir=self.get_checkpoint_dir(self.current_step, is_step=True)  # TODO: 改为按照step保存
                         unwrapped_model = self.accelerator.unwrap_model(self.model)
                         unwrapped_model.save_pretrained(save_dir,
-                                                        safe_serialization=False,
+                                                        safe_serialization=True,
                                                         accelerator=self.accelerator)
                         # self.accelerator.save_model(self.model, save_dir)
                         self.tokenizer.save_pretrained(save_dir)
