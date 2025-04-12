0a70a125ef3d24cecf9c67af31fe87a5c53de258
grok <42383434+NLPJCL@users.noreply.github.com>
2025-04-12 15:44:13 +0800

    Polish the readme

--- rag_retrieval/train/reranker/train_reranker.sh
+++ rag_retrieval/train/reranker/train_reranker.sh
@@ -10,7 +10,7 @@ if [ ! -d "./logs" ]; then
     echo "mkdir logs"
 fi
 
-# bert model,fsdp(ddp) loss_type "point_ce"
+# bert model,fsdp(ddp) 
 CUDA_VISIBLE_DEVICES="0,1" nohup accelerate launch \
 --config_file ../../../config/xlmroberta_default_config.yaml \
 train_reranker.py \
@@ -18,17 +18,9 @@ train_reranker.py \
 >./logs/training_bert.log &
 
 
-# llm model, deepspeed(zero1-2, not for zero3) loss_type "point_ce"
+# llm model, deepspeed(zero1-2, not for zero3) 
 CUDA_VISIBLE_DEVICES="0,1" nohup accelerate launch \
 --config_file ../../../config/deepspeed/deepspeed_zero1.yaml \
 train_reranker.py \
 --config config/training_llm.yaml \
 >./logs/training_llm_deepspeed1.log &
-
-
-# bert model, fsdp(ddp), distill(distill_llama_to_bert) loss_type "point_mse" or "point_ce"
-CUDA_VISIBLE_DEVICES="0,1" nohup accelerate launch \
---config_file ../../../config/xlmroberta_default_config.yaml \
-train_reranker.py \
---config config/distilling_bert.yaml \
->./logs/distilling_bert.log &
